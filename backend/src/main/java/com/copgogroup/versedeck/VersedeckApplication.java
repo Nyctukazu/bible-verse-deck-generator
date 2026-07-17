c1c8a6e9776d5ebdbc022ffe32b99242c6604e09
Nyctukazu <laurenceryancruz@gmail.com>
2026-07-17 14:10:00 +0800

    Initial commit: unified backend and frontend with working gitignore

---
+++ backend/src/main/java/com/copgogroup/versedeck/VersedeckApplication.java
@@ -0,0 +1,13 @@
+package com.copgogroup.versedeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersedeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersedeckApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/copgogroup/versedeck/VersedeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copgogroup.versedeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersedeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
